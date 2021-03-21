e577731820e0455e841d05abc07f14f5dbff2f3e
iqbalwalii <iqbalwali786@gmail.com>
2021-03-21 11:41:21 +0530

    added new cart and bugs fixed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+class Posts(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.ImageField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
 from django.views.generic import TemplateView
-
+from .models import Posts
 
 class BlogView(TemplateView):
     template_name='blog.html'
+    context_object_name='web_posts'
+    model=Posts
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+
+# class PostView(ListView):
+#     template_name='blog.html'
+    
